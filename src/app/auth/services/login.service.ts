bac8dce80c74c61ffd92928d113da5fadc814e9e
pavel asadchy <asadchy.pavel@gmail.com>
2020-09-07 16:14:20 +0300

    refactoring

--- src/app/auth/services/login.service.ts
+++ src/app/auth/services/login.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { LoginFormValue } from '../models/login-form-value.model';
 import { NavigateService } from 'src/app/shared/services/navigate.service';
-import { Observable, of, BehaviorSubject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -22,15 +22,6 @@ export class LoginService {
             JSON.stringify(loginFormValue)
             );
     }
-
-    // public getAuthToken(): any {
-    //     const savedAuthToken: any = localStorage.getItem(LoginService.authLocalStorageKey);
-    //     if (savedAuthToken) {
-    //         this.authName = JSON.parse(savedAuthToken).login;
-    //         this.isUserAuthorised = true;
-    //         return savedAuthToken;
-    //     }
-    // }
 
     public getAuthToken(): any {
         const savedAuthToken: any = localStorage.getItem(LoginService.authLocalStorageKey);
